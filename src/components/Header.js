4367c025bb66e2dad2f424dc7b1d9a5606ac3c87
Aditya Verma <aditya.verma.manit@gmail.com>
2024-03-14 10:14:14 +0530

    change margin to padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export function Header({title}) {
                 textAlign: "center",
                 fontWeight: "bolder", // Makes the text bolder
                 fontSize: "28px", // Increases the font size
-                margin: "10px 0", // Adds some space above and below the div
+                padding: "40px 0px 0px 0px", // Adds some space above and below the div
                 fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif", // Sets a custom font family
                 color: "#333", // Sets the text color to a dark gray for elegance
                 textShadow: "2px 2px 4px rgba(0,0,0,0.2)", // A more pronounced text shadow for depth
